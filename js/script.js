9318f029bd4d0ecdb2f865702607a10eab953498
YanaBrewster <yana.brewster@gmail.com>
2019-05-27 19:03:30 +1200

    about side nav fixed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-console.log("external JS is working");
+
 $(document).ready(function(){
-	console.log("jquery has loaded");
+
 })
 
 
@@ -63,57 +63,6 @@ $('.btn-hide').click(function(){
 	$('.btn-hide').hide();
 	$('.btn-show').show();
 });
-
-//
-//
-// $('#cedaw').click(function(){
-// 	$('html,body').animate({
-// 		scrollTop:$('#cedaw').offset().top},100);
-// });
-//
-// $('#reports').click(function(){
-// 	$('html,body').animate({
-// 		scrollTop:$('#reports').offset().top},100);
-// });
-//
-// $('#archive').click(function(){
-// 	$('html,body').animate({
-// 		scrollTop:$('#archive').offset().top},100);
-// });
-//
-// $('#ourhistory').click(function(){
-// 	$('html,body').animate({
-// 		scrollTop:$('#ourhistory').offset().top},100);
-// });
-//
-// $('#whoweare').click(function(){
-// 	$('html,body').animate({
-// 		scrollTop:$('#whoweare').offset().top},100);
-// });
-//
-// $('#whatwedo').click(function(){
-// 	$('html,body').animate({
-// 		scrollTop:$('#whatwedo').offset().top},100);
-// });
-
-
-
-
-
-// $('#p-whatwedo2').hide();
-// $('.btn-hide').hide();
-
-// $('.btn-show').click(function(){
-// 	$('#p-whatwedo2').slideDown(1000);
-// 	$('.btn-hide').show();
-// 	$('.btn-show').hide();
-// });
-
-// $('.btn-hide').click(function(){
-// 	$('#p-whatwedo2').slideUp();
-// 	$('.btn-hide').hide();
-// 	$('.btn-show').show();
-// });
 
 function showNav(){
     var menu = document.getElementById("navList-l");
